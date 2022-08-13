1765bf8b468761a1fe4b7b24a1cae579d2bce38c
LinaVertinskaya <linavertinskayaa@gmail.com>
2022-08-13 22:45:35 +0300

    Add my project

---
+++ CardsDesign/CardsDesign/CardsDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsDesignApp.swift
+//  CardsDesign
+//
+//  Created by Лина Вертинская on 13.08.22.
+//
+
+import SwiftUI
+
+@main
+struct CardsDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
